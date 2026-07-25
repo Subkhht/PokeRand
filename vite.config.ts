199e361e6bce3f1471efbf8ead738247b821dce8
Subkhht <mycontact.sub@gmail.com>
2026-07-25 19:25:17 +0200

    Prueba 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PokeRand/',
+  base: '/PokeRand/', // 👈 IMPORTANTE: El nombre de tu repositorio entre barras
 })
