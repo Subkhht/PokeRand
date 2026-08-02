f8fd9de909cb1b4af53f643f477df1e7800cfc4d
Subkhht <mycontact.sub@gmail.com>
2026-08-02 04:08:28 +0200

    Subida de kB

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/PokeRand/', // 👈 IMPORTANTE: El nombre de tu repositorio entre barras
+  build: {
+    chunkSizeWarningLimit: 800,
+  },
 })
